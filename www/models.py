2cedc0fd67ca9906f39fae6876d6eb1928c805c1
rm <ming_r@163.com>
2020-03-20 11:09:25 +0800

    modify orm and add models file

---
+++ www/models.py
@@ -0,0 +1,18 @@
+'Models for user, blog, comment.'
+
+__author__ = 'RuanMing'
+
+import time, uuid
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
