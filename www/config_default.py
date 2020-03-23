1c4875f1057dba6801d2125cf5b206e268af058f
rm <ming_r@163.com>
2020-03-23 22:19:06 +0800

    add config

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'default configurations'
+
+__author__ = 'RuanMing'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'seaha',
+        'password': 'password',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+'override configurations.'
+
+__author__ = 'RuanMing'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
