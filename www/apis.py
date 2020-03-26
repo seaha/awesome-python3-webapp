78da9452409ab2cf746229e148cc0a344b203b17
rm <ming_r@163.com>
2020-03-26 17:47:18 +0800

    modify

--- www/apis.py
+++ www/apis.py
@@ -30,3 +30,23 @@ class APIPermissionError(APIError):
 
     def __init__(self, message=''):
         super(APIPermissionError, self).__init__('Permission: forbidden.', 'permission', message)
+
+class Page(object):
+    'Page object for display pages.'
+
+    def __init__(self, item_count, page_index=1, page_size=10):
+        self.item_count = item_count
+        self.page_index = page_index
+        self.page_size = page_size
+        self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
+        if(item_count == 0 ) or (page_size > self.page_count):
+            self.offset = 0
+            self.limit = 0
+            self.page_size
+        self.has_next = self.page_index < self.page_count
+        self.has_previous = self.page_index > 1
+
+    def __str__(self):
+        return 'item_count: %s, page_count: %s, page_index: %s, page_size: %s, offset: %s, limit: %s' & (self.item_count,self.page_count, self.page_index, self.page_size, self.limit)
+
+    __repr__ = __str__
