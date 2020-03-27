91ce3f76c840b0130c357e6b1659d99a8e124244
rm <ming_r@163.com>
2020-03-27 11:26:39 +0800

    m

--- www/apis.py
+++ www/apis.py
@@ -39,10 +39,14 @@ class Page(object):
         self.page_index = page_index
         self.page_size = page_size
         self.page_count = item_count // page_size + (1 if item_count % page_size > 0 else 0)
-        if(item_count == 0 ) or (page_size > self.page_count):
+        if(item_count == 0 ) or (page_index > self.page_count):
             self.offset = 0
             self.limit = 0
-            self.page_size
+            self.page_size = 1
+        else:
+            self.page_index = page_index
+            self.offset = self.page_size * (page_index - 1)
+            self.limit = self.page_size
         self.has_next = self.page_index < self.page_count
         self.has_previous = self.page_index > 1
 
